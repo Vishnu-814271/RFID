0a964c6bc3abe482287bbb36db409203a63439d5
Vishnu <vishnuchityala12@gmail.com>
2026-09-03 19:24:10 +0530

    chore: remove simulation scripts and token.json from git tracking and ignore them

--- tap_in.ps1
+++
@@ -1,6 +0,0 @@
-param (
-    [Parameter(Mandatory=$false, Position=0)]
-    [string]$CardUid = "CARD_STU_2006"
-)
-
-powershell -ExecutionPolicy Bypass -File "$PSScriptRoot\simulate_tap.ps1" -CardUid $CardUid -In
